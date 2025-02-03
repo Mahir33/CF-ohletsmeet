3e31d1b42ef0f7c914bc696fa48b4e8b3a8db026
Mahir33 <adam.marek.wrobel@gmail.com>
2025-02-03 12:25:39 +0100

    implementing atatus-spa

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as atatus from 'atatus-spa';
 import App from './App.jsx'
 import './index.css'
+
+atatus.config('2627646d9e674103ac690a43b4239300').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
